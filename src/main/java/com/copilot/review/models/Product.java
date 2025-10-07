6874ae6dd836a66e2da2604b53a23e7fc4d348a4
Nalla Sriram <nallas.sriram@gmail.com>
2025-10-07 21:04:39 +0530

    test

---
+++ src/main/java/com/copilot/review/models/Product.java
@@ -0,0 +1,12 @@
+package com.copilot.review.models;
+
+import lombok.Data;
+
+@Data
+public class Product {
+    private int productId;
+    private String productName;
+    private String productDescription;
+    private int price;
+    price String dimesion;
+}
